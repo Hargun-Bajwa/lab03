89c339692c90644ec9a6ee4d29988cfdb0058d54
839217 <839217@SAIT227287IT.ACDM.DS.SAIT.CA>
2021-06-02 01:23:59 -0600

    First commit! Completed AgeCalculator files.

---
+++ src/java/servlets/ageCalculatorServlet.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 839217
+ */
+public class ageCalculatorServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request, response);
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Capture the age parameter from the post request(the form)
+        String age= request.getParameter("enteredAge");
+        
+        
+        //empty parameter check
+        if(age== null || age.equals("")){
+        request.setAttribute("message", "You must give your current age.");
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request, response);
+        return;
+        }
+        //initializing a age variable to use for validation
+        int inputAge;
+        //invalid parameter type check
+        try{inputAge=Integer.parseInt(age);}
+        catch(NumberFormatException e){
+        request.setAttribute("message", "You must enter a number.");
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request, response);
+        return;}
+        int nextBirthdayAge=Integer.parseInt(age)+1;
+        request.setAttribute("result","Your age next birthday will be "+ nextBirthdayAge);
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request, response);
+        
+      
+    }
+}
---
+++ web/WEB-INF/agecalculator.jsp
@@ -0,0 +1,26 @@
+<%-- 
+    Document   : agecalculator
+    Created on : Jun 2, 2021, 12:24:24 AM
+    Author     : 839217
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>AGE CALCULATOR</title>
+    </head>
+    <body>
+        <h1>Age Calculator</h1>
+        <form method="post" action="age"> 
+            <label>Enter your age: </label>
+            <input type="text" name="enteredAge">
+            <br />
+            <input type="submit" value="Age next birthday">
+        </form>
+        <p>${message}</p>
+        <p>${result}</p>
+        <a href="arithmeticCalculator">Arithmetic Calculator</a>
+    </body>
+</html>
